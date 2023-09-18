14c8dba16ee89411f4c3813047359332ddcba03d
Brandon Hyppolite <b.hyppolite2580@outlook.com>
2023-09-18 12:17:02 -0400

    updated tic tac toe
    removed the random player method

--- main.py
+++ main.py
@@ -10,15 +10,6 @@ class Player:
     # we want all players to get their next move given a game
     def get_move(self, game):
         pass
-
-class RandomComputerPlayer(Player):
-    def __init__(self, letter):
-        super().__init__(letter)
-
-    def get_move(self, game):
-        # get a random valid spot for our next move
-        square = random.choice(game.available_moves())
-        return square
     
 class HumanPlayer(Player):
     def __init__(self, letter):
